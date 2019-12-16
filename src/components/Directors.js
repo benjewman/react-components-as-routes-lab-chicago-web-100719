8e3b171dff00316c317758efd1db9d104b842444
benjewman <bennyadave@gmail.com>
2019-12-16 16:24:08 -0600

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -5,6 +5,16 @@ const Directors = () => {
   return (
     <div>
       {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(director => {
+        return (
+          <div>
+            <h3>{director.name}</h3>
+            <h3>Movies:</h3>
+            {director.movies.map(movie => <ul>{movie}</ul>)}
+          </div>
+        )
+      })}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,6 +5,17 @@ const Movies = () => {
   return (
     <div>
         {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => {
+          return (
+            <div>
+              <h3>Title: {movie.title}</h3>
+              <h3>Time: {movie.time}</h3>
+              <h3>Genres:</h3>
+              {movie.genres.map(genre => <ul>{genre}</ul>)}
+            </div>
+          )
+        })}
     </div>
   );
 };
